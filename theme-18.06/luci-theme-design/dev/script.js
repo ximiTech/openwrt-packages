a33c2a849499bf034e05f15beca9b128338823d0
xiangfeidexiaohuo <jsjson@163.com>
2023-04-23 22:14:57 +0800

    Auto Sync: 2023-04-23 22:14

--- theme-18.06/luci-theme-design/dev/script.js
+++ theme-18.06/luci-theme-design/dev/script.js
@@ -110,13 +110,6 @@
         }
     });
 
-    if ($("#cbi-dhcp-lan-ignore").length > 0) {
-        observer.observe(document.getElementById("cbi-dhcp-lan-ignore"), {
-            subtree: true,
-            attributes: true
-        });
-    }
-
     /**
      * hook menu click and add the hash
      */
@@ -149,9 +142,6 @@
         $("body").addClass(mainNodeName);
     }
     
-    $(".cbi-button-up").val("");
-    $(".cbi-button-down").val("");
-
     /**
      * hook other "A Label" and add hash to it.
      */
@@ -227,14 +217,6 @@
             $("header").css("box-shadow", "18rem 2px 4px rgb(0 0 0 / 8%)")
             $("header>.container>.brand").css("padding", '0rem')
         }
-    });
-   
-    /**
-     * fix legend position
-     */
-    $("legend").each(function () {
-        var that = $(this);
-        that.after("<span class='panel-title'>" + that.text() + "</span>");
     });
 
     $(".main-right").focus();
